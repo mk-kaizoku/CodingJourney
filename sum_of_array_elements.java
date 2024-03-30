640c77c4f4849222e63a1b44d5348e87d07d8a04
Merun Kumar <merunkumar88@gmail.com>
2024-03-31 00:00:48 +0530

    simple sum of array solution
    Iterate through the array and increment sum with array elements

---
+++ sum_of_array_elements.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class sum_of_array_elements {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int n= sc.nextInt();
+            long[] arr = new long[n];
+            long sum =0;
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextLong();
+            }
+            for(int i=0; i<n; i++){
+                sum+=arr[i];
+            }
+            System.out.println(sum);
+        }
+    }
+}
