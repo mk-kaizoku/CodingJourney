23491a15e6316917d7d7607b7bf19e121ee78bab
Merun Kumar <merunkumar88@gmail.com>
2024-03-31 00:34:16 +0530

    update

--- power_of_2.java
+++ power_of_2.java
@@ -8,7 +8,7 @@ public class power_of_2 {
         int t=sc.nextInt();
         while(t-->0){
             // A number if power of 2 if the binary form of the number contains only one set bit
-            // Si   x   bi      x-1     bi      x&x-1   bi
+            // Si       x   bi      x-1     bi      x&x-1   bi
             // 1	8	1000	7	    0111	0	    0000
             // 2	6	0110	5	    0101	4	    0100
             // 3	16	10000	15	    01111	0	    00000
