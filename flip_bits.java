0ae34a79ea33261a1b7f6125820843c2bd365810
Merun Kumar <merunkumar88@gmail.com>
2024-03-31 00:55:02 +0530

    xor method

---
+++ flip_bits.java
@@ -0,0 +1,28 @@
+// https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/flip-bits?page=0&pageSize=50
+
+import java.io.*;
+import java.util.*;
+
+public class flip_bits {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            int b= sc.nextInt();
+            // using xor operation to check no of bits required to be shifted 
+            // counting no of set bits and returning the count 
+            // a=20 b=10  
+            // 10100  01010  a^b=11110  
+            int x=a^b;
+            int count=0;
+            while(x>0){
+                if ((x&1)==1)
+                    count++;
+                x>>=1;
+            }
+            System.out.println(count);
+        }
+    }
+}
