6838a5995cb84a89d8a03f1b9c4601c456b64762
Merun Kumar <merunkumar88@gmail.com>
2024-03-31 00:33:05 +0530

    bit manipulation technique
    using x&x-1 if 0 eq power of 2 else not

---
+++ power_of_2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class power_of_2 {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            // A number if power of 2 if the binary form of the number contains only one set bit
+            // Si   x   bi      x-1     bi      x&x-1   bi
+            // 1	8	1000	7	    0111	0	    0000
+            // 2	6	0110	5	    0101	4	    0100
+            // 3	16	10000	15	    01111	0	    00000
+            long x=sc.nextLong();
+            if((x&(x-1))!=0)
+                System.out.println("False");
+            else
+                System.out.println("True");
+        }
+    }
+}
