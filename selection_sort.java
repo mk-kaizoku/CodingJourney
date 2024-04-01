e98072e695f1d6fb597f1299cec6ca687cada367
Merun Kumar <merunkumar88@gmail.com>
2024-04-01 18:37:37 +0530

    -_-
    return indices which gets swapped each time

---
+++ selection_sort.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class selection_sort {
+    public static void selection_sort(int[] arr, int n){
+        for(int i=0; i<n-1; i++){
+            int max=0;
+            for(int j=1; j<n-i; j++){
+                if(arr[j]>arr[max]){
+                    max=j;
+                }
+            }
+            System.out.print(max+" ");
+            int temp=arr[max];
+            arr[max]=arr[n-1-i];
+            arr[n-1-i]=temp;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc =new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            selection_sort(arr,n);
+        }
+    }
+}
