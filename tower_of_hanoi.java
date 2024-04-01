79b6b07b87daf0a6c5ac45c0f246b981a6c04aa0
Merun Kumar <merunkumar88@gmail.com>
2024-04-01 18:30:34 +0530

    Create tower_of_hanoi.java
    if n=1
    move src to dest
    else
    n-1 src aux dest
    move src to dest
    n-1 aux dest src

---
+++ tower_of_hanoi.java
@@ -0,0 +1,30 @@
+// https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/tower-of-hanoi?page=0&pageSize=100
+import java.io.*;
+import java.util.*;
+
+public class tower_of_hanoi {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int result=(int) Math.pow(2,n)-1;
+            System.out.println(result);
+            hanoitowerR(n,'A','C','B');
+
+        }
+    }
+    
+    public static void hanoitowerR(int n,char src, char dest, char aux){
+        if(n==1){
+            System.out.println("Move 1 from "+src+" to "+dest);
+            return;
+        }else{
+            hanoitowerR(n-1,src,aux,dest);
+            System.out.println("Move "+n+" from "+src+" to "+dest);
+            hanoitowerR(n-1,aux,dest,src);            
+        }
+    }
+}
