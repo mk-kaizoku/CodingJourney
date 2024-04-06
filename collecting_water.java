21021ec4b0ae9c8fe793aa149bd2a87a9fb1d595
Merun Kumar <merunkumar88@gmail.com>
2024-04-06 14:41:39 +0530

    two pointer approach
    other approaches is to create prefix and suffix sum arrays result+=Math.min(preifx[i],suffix[i])-arr[i];

---
+++ collecting_water.java
@@ -0,0 +1,45 @@
+// https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/collecting-water?page=0&pageSize=100
+
+import java.io.*;
+import java.util.*;
+
+public class collecting_water {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr= new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            System.out.println(collectwater(arr,n));
+        }
+    }
+    public static int collectwater(int[] arr, int n){
+        int left=0;
+        int right =n-1;
+        int leftmax=0;
+        int rightmax=0;
+        int result=0;
+        while(left<=right){
+            if(arr[left]<=arr[right]){
+                if(arr[left]>=leftmax)
+                    leftmax=arr[left];
+                else
+                    result+=leftmax-arr[left];
+                left++;
+            }
+            else{
+                if(arr[right]>=rightmax)
+                    rightmax=arr[right];
+                else
+                    result+=rightmax-arr[right];
+                right--;
+            }
+
+        }
+        return result;
+    }
+}
